eff6de1e2ffc0ed5fa06f680879f104e3044c9bb
Jethro Baring <jyty.dev@gmail.com>
2022-06-22 01:57:02 +0800

    playGround function done

--- rps.js
+++ rps.js
@@ -3,3 +3,16 @@ let computerPlay = () => {
     return random[Math.floor(Math.random()*3)]
 }
 
+let playGroud = (playerSelection, computerSelection) => {
+    if(playerSelection.toLowerCase() == "scissor" && computerSelection == "paper"){
+        return `${playerSelection.toLowerCase()} beats ${computerSelection}. You win!!!`
+    }else if(playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
+        return `${playerSelection.toLowerCase()} beats ${computerSelection}. You win!!!`
+    }else if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissor"){
+        return `${playerSelection.toLowerCase()} beats ${computerSelection}. You win!!!`
+    }else if(playerSelection.toLowerCase() == computerSelection){
+        return `${playerSelection.toLowerCase()} and ${computerSelection} are the same. It's a DRAW.`
+    }else{
+        return `${playerSelection.toLowerCase()} is weak agains ${computerSelection}. You lose!!!`
+    }
+}
