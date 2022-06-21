e8dec9e5fd25eef34e0a366e3144b2e6321b5913
Jethro Baring <jyty.dev@gmail.com>
2022-06-22 02:20:40 +0800

    game function done

--- rps.js
+++ rps.js
@@ -1,18 +1,41 @@
 let computerPlay = () => {
-    let random = ['rock','paper','scissor']
-    return random[Math.floor(Math.random()*3)]
+    let random = ['rock','paper','scissor'];
+    return random[Math.floor(Math.random()*3)];
 }
 
 let playGround = (playerSelection, computerSelection) => {
     if(playerSelection.toLowerCase() == "scissor" && computerSelection == "paper"){
-        return `${playerSelection.toLowerCase()} beats ${computerSelection}. You win!!!`
+        return "You Win!"
     }else if(playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
-        return `${playerSelection.toLowerCase()} beats ${computerSelection}. You win!!!`
+        return "You Win!"
     }else if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissor"){
-        return `${playerSelection.toLowerCase()} beats ${computerSelection}. You win!!!`
+        return "You Win!"
     }else if(playerSelection.toLowerCase() == computerSelection){
-        return `${playerSelection.toLowerCase()} and ${computerSelection} are the same. It's a DRAW.`
+        return "Draw"
     }else{
-        return `${playerSelection.toLowerCase()} is weak agains ${computerSelection}. You lose!!!`
+        return "You Lose!"
     }
 }
+
+let game = () => { 
+    let user = 0;
+    let computer = 0;
+    for(let i = 0; i < 5; i++){
+        let user = prompt("Choose: ");
+        let result = playGround(user,computerPlay())
+
+        if(result == "You Win!"){
+            console.log("You win the round!")
+            user++;
+        }else if(result == "You Lose!"){
+            console.log("Computer win the round!")
+            computer++;
+        }else{
+            console.log("Its a draw!")
+        }
+    }
+
+    user > computer? console.log("You win the game"): console.log("Computer win the game")
+}
+
+game()
