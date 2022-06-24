d961574bcb129661c6836b92897042874f9d34c5
Jethro Baring <jyty.dev@gmail.com>
2022-06-25 03:40:41 +0800

    done coding the logic of the game

--- rps.js
+++ rps.js
@@ -19,7 +19,7 @@ let playGround = (playerSelection, computerSelection) => {
 
 let game = () => { 
     let user = 0;
-    let computer = 0;
+    let computerSelection = 0;
     for(let i = 0; i < 5; i++){
         let user = prompt("Choose: ");
         let result = playGround(user,computerPlay())
@@ -29,19 +29,55 @@ let game = () => {
             user++;
         }else if(result == "You Lose!"){
             console.log("Computer win the round!")
-            computer++;
+            computerSelection++;
         }else{
             console.log("Its a draw!")
         }
     }
 
-    user > computer? console.log("You win the game"): console.log("Computer win the game")
+    user > computerSelection? console.log("You win the game"): console.log("Computer win the game")
 }
 
 game() */
 
-const rock = document.querySelector('rock')
+const startButton = document.querySelector(".startButton");
+const buttons = document.querySelectorAll(".choices");
 
-function al(){
-    alert("Hello World")
+let computerPlay = () => {
+  let random = ["rock", "paper", "scissor"];
+  return random[Math.floor(Math.random() * 3)];
+};
+
+let player = () => {
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            startGame(button.id,computerPlay())
+        });
+    });
+};
+
+let startGame = (playerSelection, computerSelection) => {
+    if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        document.querySelector('')
+    }else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        console.log('You Win')
+    }else if(playerSelection === 'scissor' && computerSelection == 'paper'){
+        console.log('You Win')
+    }else if(playerSelection === computerSelection){
+        console.log('Draw')
+    }else{
+        console.log('You Lose')
+    }
+}
+
+let game = () => {
+    document.querySelector("h1").innerText = "Choose your weapon";
+    player();
+}
+
+
+
+
+let choose = () => {
+    game()
 }
